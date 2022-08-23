e7a6a2f84d4d9a628497686500b564e5ab4658cc
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2022-08-23 11:47:29 +0200

    offers index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "offers#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/offers', to: 'offers#index'
 end
