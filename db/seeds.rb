dbd03d9d8d86017101b7558c080e269a61140496
Carlos Baez Fumero <carlos.baez.fumero@gmail.com>
2022-08-24 11:02:28 +0200

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants..."
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.city, category: "chinese")
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
