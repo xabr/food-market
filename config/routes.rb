c017041991bb4b4355e8efe594d5a49f75f650a0
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2022-08-25 15:42:30 +0200

    profile page exist

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/offers', to: 'offers#index'
   get '/offer/new', to: 'offers#new'
-  get 'offers/:id', to: 'offers#show', as: :offer
-  delete 'offers/:id', to: 'offers#destroy'
+  get '/offers/:id', to: 'offers#show', as: :offer
+  delete '/offers/:id', to: 'offers#destroy'
   post '/offers', to: 'offers#create'
   get '/offers/:id/new', to: 'orders#new'
   get '/offers', to: 'orders#create'
+  get '/users/:id', to: 'users#show', as: :users
+  get '/users/:id/orders', to: 'orders#show', as: :orders
 end
